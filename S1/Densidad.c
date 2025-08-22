42f1d3e55288032c13b648602678c247daec64aa
cormac434 <cormac434@gmail.com>
2025-08-21 19:34:04 -0600

    subir proyecto hecho en claso

---
+++ S1/Densidad.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+    float masa, volumen, densidad;
+
+    printf("Ingresa el valor de la masa: ");
+    scanf("%f",&masa);
+    printf("Ingresa el valor del volumen");
+    scanf("%f", &volumen);
+
+    densidad= masa/volumen;
+
+    if(densidad<=1){
+        printf("se hunde\n");
+        }
+    else{
+    printf("flota\n");
+    }
+getch();
+}
